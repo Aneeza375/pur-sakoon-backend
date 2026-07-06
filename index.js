da7369fec2a9dd12d52bde2019d36a94aafc30d0
Aneeza375 <b-28277@student.usa.edu.pk>
2026-07-07 00:23:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import app from "./server/app.js";
 import dotenv from 'dotenv';
 
+dotenv.config({
+    path: "./.env"
+});
 
-dotenv.config(
-    {
-        path: "./.env"
-    }
-);
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+export default app;
 })
